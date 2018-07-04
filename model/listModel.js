4f136be139f33036c25c068c44057db8e7e842ce
Paynecheng <PayneChen@yeah.net>
2018-07-04 20:51:25 +0800

    modify

--- model/listModel.js
+++ model/listModel.js
@@ -2,9 +2,11 @@ const db = require('../config/db');
 
 const model = {
     list: async (id) => {
-        const sql = 'SELECT * FROM article where category_id = ?';
-        return await db.query(sql, [id]);
+        const sql = "SELECT article_id, c.name as 'category', title, description, last_date, views " +
+        "FROM article as a, category as c " +
+        "WHERE a.category_id = ? AND c.category_id = a.category_id ";
+        return await db.query(sql, [id])
     }
-}
+};
 
 module.exports = model;
