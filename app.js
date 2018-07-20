dfd07116a1ba40ca8273fbb67fffabefe87e5e4f
chenwenjian <chenwenjian@leadeon.cn>
2018-07-20 15:20:31 +0800

    tt

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 
-
+app.use('/', indexRouter);
 app.use('/frontEnd', indexRouter);
 app.use('/admin', adminRouter);
 app.use('/list', listRouter);
